28d0c63035b2cb84d295deac096ac514cfdb12af
Md Rezaul Karim <iosmkarim@gmail.com>
2024-12-18 13:44:52 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Md Rezaul Karim on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
